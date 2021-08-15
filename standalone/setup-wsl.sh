114c231c0d5085d761debbe7d39cfd9ae94bd08f
icyzeroice <ziv3@outlook.com>
2021-08-15 10:11:51 +0800

    add setup-wsl.sh

---
+++ standalone/setup-wsl.sh
@@ -0,0 +1,29 @@
+# 1. Setup github ssh key
+# git clone https://gitee.com/icyzeroice/ssh-save.git .ssh
+
+
+
+# 2. Setup Tmux
+# https://github.com/gpakosz/.tmux
+cd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+
+# 3. Setup Windows Terminal starting directory
+# https://superuser.com/questions/1185033/what-is-the-home-directory-on-windows-subsystem-for-linux
+# \\wsl$\Ubuntu\home\hyouka
+
+
+# 4. Setup Chinese
+# https://docs.microsoft.com/en-us/windows/wsl/faq#how-do-i-change-the-display-language-of-wsl-
+
+# 5. Spacemacs
+cd ~
+sudo apt install sqlite
+git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone git@github.com:icyzeroice/.spacemacs.d.git
+
+# End the file
+
