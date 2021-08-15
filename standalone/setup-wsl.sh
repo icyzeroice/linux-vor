fbd1c43f6d5718b7738e8cbdbff8f5a1a0bc81be
icyzeroice <ziv3@outlook.com>
2021-08-15 16:05:13 +0800

    add wslg chinese

--- standalone/setup-wsl.sh
+++ standalone/setup-wsl.sh
@@ -30,5 +30,12 @@ sudo apt install sqlite
 git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone git@github.com:icyzeroice/.spacemacs.d.git
 
+
+# 6. WSLG Chinese
+# 这个我在没安装中文字体的时候试了，不太行，会绿屏：https://blog.csdn.net/defrag257/article/details/104029924
+# 这个我在安装中文字体后试了，可以：https://blog.csdn.net/xie1xiao1jun/article/details/54377946
+
+
+
 # End the file
 
