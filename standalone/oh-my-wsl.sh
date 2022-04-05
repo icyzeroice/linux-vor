f8be1739a74aaf1217d341e3fd6962b0767ffb6d
icyzeroice <ziv3@outlook.com>
2022-04-05 10:33:18 +0800

    fix variable name typo.

--- standalone/oh-my-wsl.sh
+++ standalone/oh-my-wsl.sh
@@ -1,8 +1,10 @@
 # https://jiayaoo3o.github.io/2020/06/23/记录一次WSL2的网络代理配置/
 
-export window_host=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
-# export ALL_PROXY=socks5://$window_host:10808
-export ALL_PROXY=http://$window_host:10809
+# NOTE: socks   -> 10808
+#       http(s) -> 10809
+export windows_host=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
+# export ALL_PROXY=socks5://$windows_host:10808
+export ALL_PROXY=http://$windows_host:10809
 export HTTP_PROXY=$ALL_PROXY
 export http_proxy=$ALL_PROXY
 export HTTPS_PROXY=$ALL_PROXY
@@ -14,7 +16,10 @@ export EDITOR=vim
 export BROWSER=wslview
 
 
-if [ "`git config --global --get proxy.https`" != "socks5://$windows_host:10808" ]
-then
-  git config --global proxy.https socks5://$windows_host:10808
-fi
+# if [ "`git config --global --get proxy.https`" != "socks5://$windows_host:10808" ]
+# then
+#   git config --global proxy.https socks5://$windows_host:10808
+# fi
+
+git config --global proxy.https socks5://$windows_host:10808
+
